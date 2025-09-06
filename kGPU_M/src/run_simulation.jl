f06c6bfd78328b927de1f8a1f98cb162ac074114
Cheuk Hei Hayden Cheung <cheunghayden09@gmail.com>
2025-09-06 18:45:08 +0100

    v0.2.0

---
+++ kGPU_M/src/run_simulation.jl
@@ -0,0 +1,58 @@
+import CUDA
+
+
+include("typedefs.jl")
+include("Fourier_array.jl")
+include("solver.jl")
+include("convection.jl")
+
+
+function update(solver1::solver)
+    calc_conv(solver1, solver1.ω_hat_prev, solver1.conv_prev);
+    solver1.ω_hat_intermediate .= solver1.Ginv_1 .* (solver1.ω_hat_prev .+ solver1.dt .* (-0.5 .* solver1.G .* solver1.ω_hat_prev .- solver1.conv_prev .+ solver1.source_hat));
+    
+    calc_conv(solver1, solver1.ω_hat_intermediate, solver1.conv_intermediate);
+
+    solver1.ω_hat_new .= solver1.Ginv_1 .* (solver1.ω_hat_prev .+ solver1.dt .* (-0.5 .* solver1.G .* solver1.ω_hat_prev .- 0.5 .* (solver1.conv_prev .+ solver1.conv_intermediate) .+ solver1.source_hat));
+
+    return nothing;
+end
+
+function iter(solver1::solver, iteration::Int64)
+    update(solver1);
+
+    CUDA.copyto!(solver1.ω_hat_prev, solver1.ω_hat_new);
+
+    CUDA.CUBLAS.mul!(solver1.ω_temp, solver1.irfft_plan_padded, solver1.ω_hat_new);
+    view(solver1.ω_arr, 1:solver1.x_len, 1:solver1.y_len, iteration) .= solver1.ω_temp;
+    return nothing;
+end;
+
+function iter(solver1::solver, measures::flow_measures, iteration::Int64)
+    update(solver1);
+
+    CUDA.copyto!(solver1.ω_hat_prev, solver1.ω_hat_new);
+
+    CUDA.CUBLAS.mul!(solver1.ω_temp, solver1.irfft_plan_padded, solver1.ω_hat_new);
+    view(solver1.ω_arr, 1:solver1.x_len, 1:solver1.y_len, iteration) .= solver1.ω_temp;
+
+    calc_flow_measures_iter(solver1, measures, iteration);
+
+    return nothing;
+end;
+
+
+function run(solver1::solver, num_iters::Int64)
+    for i in 1:num_iters
+        iter(solver1, solver1.current_iter);
+        solver1.current_iter += 1;
+    end;
+end;
+
+
+function run(solver1::solver, measures::flow_measures, num_iters::Int64)
+    for i in 1:num_iters
+        iter(solver1, measures, solver1.current_iter);
+        solver1.current_iter += 1;
+    end;
+end;
---
+++ kGPU_M/src/typedefs.jl
@@ -0,0 +1,25 @@
+import AbstractFFTs
+import CUDA
+import FFTW
+
+complex_type = Union{ComplexF32, ComplexF64}
+float_type = Union{Float16, Float32, Float64}
+
+arr_type = Union{Array, CUDA.CuArray};
+rfft_type = Union{FFTW.rFFTWPlan{Float64, -1, false, 2, Tuple{Int64, Int64}}, 
+                    CUDA.CUFFT.CuFFTPlan{ComplexF32, Float32, -1, false, 2, 2, CUDA.CuArray{ComplexF32, 2, CUDA.DeviceMemory}},
+                    CUDA.CUFFT.CuFFTPlan{ComplexF64, Float64, -1, false, 2, 2, CUDA.CuArray{ComplexF64, 2, CUDA.DeviceMemory}}};
+irfft_type = Union{AbstractFFTs.ScaledPlan{ComplexF64, FFTW.rFFTWPlan{ComplexF64, 1, false, 2, UnitRange{Int64}}, Float64},
+    #AbstractFFTs.ScaledPlan{ComplexF32, CUDA.CUFFT.CuFFTPlan{Float32, ComplexF32, 1, false, 2, 2, CUDA.CuArray{ComplexF32, 2, CUDA.DeviceMemory}}, Float32},
+    AbstractFFTs.ScaledPlan{ComplexF64, CUDA.CUFFT.CuFFTPlan{Float64, ComplexF64, 1, false, 2, 2, CUDA.CuArray{ComplexF64, 2, CUDA.DeviceMemory}}, Float64}};
+
+CPU_rfft_type32 = FFTW.rFFTWPlan{Float32, -1, false, 2, Tuple{Int32, Int32}};
+CPU_rfft_type64 = FFTW.rFFTWPlan{Float64, -1, false, 2, Tuple{Int64, Int64}};
+CPU_irfft_type32 = AbstractFFTs.ScaledPlan{ComplexF32, FFTW.rFFTWPlan{ComplexF32, 1, false, 2, UnitRange{Int32}}, Float32};
+CPU_irfft_type64 = AbstractFFTs.ScaledPlan{ComplexF64, FFTW.rFFTWPlan{ComplexF64, 1, false, 2, UnitRange{Int64}}, Float64};
+
+GPU_rfft_type32 = CUDA.CUFFT.CuFFTPlan{ComplexF32, Float32, -1, false, 2, 2, CUDA.CuArray{ComplexF32, 2, CUDA.DeviceMemory}};
+GPU_rfft_type64 = CUDA.CUFFT.CuFFTPlan{ComplexF64, Float64, -1, false, 2, 2, CUDA.CuArray{ComplexF64, 2, CUDA.DeviceMemory}};
+GPU_irfft_type32 = AbstractFFTs.ScaledPlan{ComplexF64, CUDA.CUFFT.CuFFTPlan{Float32, ComplexF32, 1, false, 2, 2, CUDA.CuArray{ComplexF32, 2, CUDA.DeviceMemory}}, Float32}
+GPU_irfft_type64 = AbstractFFTs.ScaledPlan{ComplexF64, CUDA.CUFFT.CuFFTPlan{Float64, ComplexF64, 1, false, 2, 2, CUDA.CuArray{ComplexF64, 2, CUDA.DeviceMemory}}, Float64};
+
